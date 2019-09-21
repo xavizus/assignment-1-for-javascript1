e0220d0e96652d756bd98cac2519a672bb872bf7
Stephan Ljungros <stephan@xavizus.com>
2019-09-21 01:29:04 +0100

    Added tonnes of new features. - Start screen with interaction from the user - Change start screen to game screen. - Added a menu that's dynamic changed depending on input from start screen - Added some animation to the design, I am not completly happy about it. But it's good enough.

--- js/Quiz.js
+++ js/Quiz.js
@@ -1,9 +1,30 @@
 class Quiz {
 
     constructor(addedName,sumOfQuestionsToAdd) {
+
+        try {
+            if(isNaN(sumOfQuestionsToAdd)) {
+                throw new Error(`sumOfQuestionsToAdd is not a number! You gave ${sumOfQuestionsToAdd}`);
+            } 
+            else if (sumOfQuestionsToAdd === "") {
+                throw new Error(`sumOfQuestionsToAdd is empty!`);
+            }   
+            else {
+                this.sumOfQuestionsToAdd = Number(sumOfQuestionsToAdd);
+            }
+        }
+        catch(error) {
+            console.error(error);
+            console.log("Setting sumOfQuestionsToAdd to value: 1");
+            this.sumOfQuestionsToAdd = 1;
+        }
+
         this.userName = addedName;
         this.questions = [];
-        this.sumOfQuestionsToAdd = sumOfQuestionsToAdd;
+        this.countCorrectGuesses = 0;
+        this.countWrongGuesses = 0;
+    
+        this.generateQuestions();
     }
 
     generateQuestions() {
@@ -24,4 +45,13 @@ class Quiz {
     checkIfAnswersAreCorrect() {
         
     }
+
+    getTotalAnsweredQuestions() {
+        return this.countCorrectGuesses + this.countWrongGuesses;
+    }
+
+    getTotalQuestions() {
+        return this.questions.length;
+    }
+
 }
--- js/main.js
+++ js/main.js
@@ -1,4 +1,124 @@
-let quiz = new Quiz("Stephan", 5);
-quiz.generateQuestions();
 
-console.log(quiz);
+function getElementsValues(elements) {
+
+    try {
+
+        //For safty reason, always convert DOM-elements to an array.
+        elements = Array.from(elements);
+
+        //Do we trust ourselves? Nope, always check if our parameter is an array
+        if (!Array.isArray(elements)) {
+
+            throw Error("You did not input an array!");
+
+        }
+
+        let results = [];
+
+        for (let element of elements) {
+
+            results.push(element.value);
+
+        }
+
+        return results;
+
+    } catch (error) {
+
+        console.error(error);
+
+        return false;
+
+    }
+}
+
+function isElementsValueEmpty(elements) {
+
+    let results = getElementsValues(elements);
+
+    if(results === false) {
+
+        return false;
+
+    }
+
+    for (let result of results) {
+
+        if (result == "") {
+
+            return false;
+
+        }
+
+    }
+
+    return true
+
+}
+
+function toggleHideShowElements(elements, hideClass = "hide", showClass="show") {
+
+    elements = Array.from(elements);
+
+    for(let element of elements) {
+
+        if(element.classList.contains("hide")) {
+
+            document.getElementById(element.id).classList.remove(hideClass);
+
+            document.getElementById(element.id).classList.add(showClass);
+
+        }
+        else {
+
+            document.getElementById(element.id).classList.add(hideClass);
+
+            document.getElementById(element.id).classList.remove(showClass);
+
+        }
+
+    }
+
+}
+
+function startQuiz(startFormElements) {
+
+    let [userName, numberOfQuestions] = getElementsValues(startFormElements);
+
+    let quiz = new Quiz(userName, numberOfQuestions);
+
+    let contentToChange = document.getElementsByClassName("quizContent");
+
+    toggleHideShowElements(contentToChange);
+
+    for(let index in quiz.questions) {
+        let questionProgress = document.createElement("input");
+        questionProgress.setAttribute("type","checkbox");
+        questionProgress.setAttribute("name","questionOverview");
+        questionProgress.setAttribute("id",`question-${index}`);
+        questionProgress.setAttribute("value",index);
+        questionProgress.setAttribute("disabled","");
+        document.getElementById("questionsOverview").append(questionProgress);
+    }
+    console.log( document.getElementById("countOverview"));
+    
+    document.getElementById("countOverview").textContent =`${quiz.getTotalAnsweredQuestions()} / ${quiz.getTotalQuestions()} frågor`;
+
+}
+
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    document.getElementById("startTheQuiz").addEventListener("click", () => {
+
+        let startFormElements = document.forms["start_form"].getElementsByClassName("start_Form");
+
+        if (isElementsValueEmpty(startFormElements)) {
+
+            startQuiz(startFormElements);
+
+        }
+
+    });
+
+});
